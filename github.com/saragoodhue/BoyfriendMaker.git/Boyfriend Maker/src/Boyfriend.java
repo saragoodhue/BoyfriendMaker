f79b63ae1eb6bf779dbbfccc5eb95a33c065b1f7
sara.goodhue <sara.goodhue@MU-C1-12.mullen.pvt.k12.co.us>
2022-05-10 13:11:28 -0600

    Boyfriend Maker

---
+++ github.com/saragoodhue/BoyfriendMaker.git/Boyfriend Maker/src/Boyfriend.java
@@ -0,0 +1,17 @@
+
+public class Boyfriend
+	{
+
+		String name;
+		double height;
+		boolean isRich;
+
+
+		public Boyfriend(String n, double h, boolean r)
+		{
+			name = n;
+			height = h;
+			isRich = r;
+		}
+
+	}
